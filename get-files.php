01a315a2491fe1c6ee42bc609385ea69a64d88c7
Gopal Joshi <gopal.joshi73@hotmail.com>
2018-01-01 19:39:22 +0530

    Initial Commit

---
+++ get-files.php
@@ -0,0 +1,23 @@
+<?
+  
+  require('google-client.php');
+
+  // Get the API client and construct the service object.
+  $client = getClient();
+  $service = new Google_Service_Drive($client);
+
+  // Print the names and IDs for up to 10 files.
+  $optParams = array(
+    'pageSize' => 10,
+    'fields' => 'nextPageToken, files(id, name)'
+  );
+  $results = $service->files->listFiles($optParams);
+
+  if (count($results->getFiles()) == 0) {
+    print "No files found.\n";
+  } else {
+    print "Files:\n";
+    foreach ($results->getFiles() as $file) {
+      printf("%s (%s)\n", $file->getName(), $file->getId());
+    }
+  }
---
+++ google-client.php
@@ -0,0 +1,71 @@
+<?php
+  require_once __DIR__ . '/vendor/autoload.php';
+
+  define('APPLICATION_NAME', 'List Google Drive Files');
+  define('CREDENTIALS_PATH', 'secrets\drive-php.json');
+  define('CLIENT_SECRET_PATH', __DIR__ . '\secrets\client_secret.json');
+
+
+  // If modifying these scopes, delete your previously saved credentials
+  define('SCOPES', implode(' ', array(
+    Google_Service_Drive::DRIVE_METADATA_READONLY)
+  ));
+
+  if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+  }
+
+  /**
+   * Returns an authorized API client.
+   * @return Google_Client the authorized client object
+   */
+  function getClient() {
+    $client = new Google_Client();
+    $client->setApplicationName(APPLICATION_NAME);
+    $client->setScopes(SCOPES);
+    $client->setAuthConfig(CLIENT_SECRET_PATH);
+    $client->setAccessType('offline');
+
+    // Load previously authorized credentials from a file.
+    $credentialsPath = expandHomeDirectory(CREDENTIALS_PATH);
+    if (file_exists($credentialsPath)) {
+      $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    } else {
+      // Request authorization from the user.
+      $authUrl = $client->createAuthUrl();
+      printf("Open the following link in your browser:\n%s\n", $authUrl);
+      print 'Enter verification code: ';
+      $authCode = trim(fgets(STDIN));
+
+      // Exchange authorization code for an access token.
+      $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+      // Store the credentials to disk.
+      if(!file_exists(dirname($credentialsPath))) {
+        mkdir(dirname($credentialsPath), 0700, true);
+      }
+      file_put_contents($credentialsPath, json_encode($accessToken));
+      printf("Credentials saved to %s\n", $credentialsPath);
+    }
+    $client->setAccessToken($accessToken);
+
+    // Refresh the token if it's expired.
+    if ($client->isAccessTokenExpired()) {
+      $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+      file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+  }
+
+  /**
+   * Expands the home directory alias '~' to the full path.
+   * @param string $path the path to expand.
+   * @return string the expanded path.
+   */
+  function expandHomeDirectory($path) {
+    $homeDirectory = getenv('HOME');
+    if (empty($homeDirectory)) {
+      $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+    }
+    return str_replace('~', realpath($homeDirectory), $path);
+  }
